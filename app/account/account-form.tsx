a146147eb942b124747c2475c042eb495b94bafd
SpaceCafard <123333174+SpaceCafard@users.noreply.github.com>
2025-03-07 11:55:56 +0000

    logging

--- app/account/account-form.tsx
+++ app/account/account-form.tsx
@@ -3,7 +3,8 @@ import { useCallback, useEffect, useState } from 'react'
 import { createClient } from '@/utils/supabase/client'
 import { type User } from '@supabase/supabase-js'
 
-// ...
+console.log('NEXT_PUBLIC_SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL)
+console.log('NEXT_PUBLIC_SUPABASE_ANON_KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 
 export default function AccountForm({ user }: { user: User | null }) {
   const supabase = createClient()
